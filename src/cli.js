7c8ace09c3b4759d560f887be452030a9067858e
Nikolay_Bagrov <nbagrovv@gmail.com>
2023-11-09 11:39:44 +0300

    add readline-sync

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export default (name) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
+  return console.log("Hello " + userName + "!");
+};
+
