c367cd07b3340e8a23931abe5d0aaf359ddb4542
Nikolay_Bagrov <nbagrovv@gmail.com>
2023-11-10 10:55:56 +0300

    add files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ratibor60/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ratibor60/frontend-project-44/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/d21124f2970d5ce841d1/maintainability)](https://codeclimate.com/github/ratibor60/frontend-project-44/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/d21124f2970d5ce841d1/test_coverage)](https://codeclimate.com/github/ratibor60/frontend-project-44/test_coverage)
---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.4"
+    id("com.github.ben-manes.versions") version "0.49.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "io.hexlet"
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("io.hexlet.Application") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.13.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
